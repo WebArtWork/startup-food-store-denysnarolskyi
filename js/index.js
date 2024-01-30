c184a852ce83b1640ffec47b7ab7560b72f2c3fe
Narolskyi Denis <asotizianov@gmail.com>
2024-01-30 17:07:21 +0200

    initialize products structure

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+console.log("Hello World");
+function showDetails(detailsId) {
+  var detailsContainers = document.querySelectorAll(".details-container");
+  detailsContainers.forEach(function (container) {
+    container.classList.remove("active");
+  });
+
+  document.getElementById(detailsId).classList.add("active");
+
+  var listItems = document.querySelectorAll(".list-item");
+  listItems.forEach(function (item) {
+    item.classList.remove("active");
+  });
+
+  event.currentTarget.classList.add("active");
+}
